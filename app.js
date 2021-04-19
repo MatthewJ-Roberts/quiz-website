db9f68bcde093e0a9195d45e3569614bd14cfc20
Matthew Roberts <matthewjroberts2001@gmail.com>
2021-04-19 18:38:57 -0300

    Added responsive background colours.

--- app.js
+++ app.js
@@ -2,10 +2,28 @@
 
 function readInput() {
     var input = document.getElementById("userInput").value;
+    var answer = document.body;
     
+    console.log(document.body.className);
     if (input != 4) {
+
+        if (document.body.className == "default correct") {
+            answer.classList.toggle("correct");
+        }
+        
+        if (document.body.className != "default incorrect"){
+            answer.classList.toggle("incorrect");
+        }
         alert("Incorrect!");
     } else {
+
+        if (document.body.className == "default incorrect") {
+            answer.classList.toggle("incorrect");
+        }
+
+        if (document.body.className != "default correct") {
+            answer.classList.toggle("correct");
+        }
         alert("Correct!");
     }
 
