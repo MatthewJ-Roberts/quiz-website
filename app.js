aea8a15a0f4334b769d7a7a43574d3567d75a5f2
Matthew Roberts <matthewjroberts2001@gmail.com>
2021-04-19 18:06:36 -0300

    Made the base form of the website.

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+function readInput() {
+    var input = document.getElementById("userInput").value;
+    
+    if (input != 4) {
+        alert("Incorrect!");
+    } else {
+        alert("Correct!");
+    }
+
+}
